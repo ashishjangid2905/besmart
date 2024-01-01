48baf79b86ab78c1991e77e260997ada87b6b3ed
ashishjangid2905 <ashish.jangid2905@outlook.com>
2024-01-01 13:10:31 +0530

    Accordion Add

--- myapp/static/js/script.js
+++ myapp/static/js/script.js
@@ -1,16 +1,10 @@
 $(document).ready(function () {
     $(".tab_head").click(function () {
-        var col = $('.tab_head').length;
-        var active = $(this);
-        var t = active.length
-        var b;
-        active.next().toggleClass('expand');
-        console.log(col);
-        console.log(t);
+        if ($(this).attr('class').indexOf('expand') == -1) 
+        
+        $(this).toggleClass("expand").next().slideToggle('fast')
 
-        for (let i = 0; i < col.length; i++) {
-            b = i;
-        }
-        console.log(b);
+        // $(".tab").not($(this)).removeClass("open");
+        $(".collap").not($(this).next()).slideUp('fast');
     });
 });
