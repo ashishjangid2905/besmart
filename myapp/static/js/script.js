53474bfa6d93a80be15a390e3ac5043ac08f0c6d
ashishjangid2905 <ashish.jangid2905@outlook.com>
2024-01-17 18:38:50 +0530

    trying to add testimonial slides

--- myapp/static/js/script.js
+++ myapp/static/js/script.js
@@ -1,16 +1,50 @@
-$(document).ready(function () {
-    $(".tab_head").click(function () {
-        if ($(this).next().hasClass('expand')) {
-            $(this).next().removeClass('expand');
-        } 
-        
-        else if($(this).next().siblings().hasClass('expand')) {
-            $(this).next().siblings().removeClass('expand');
-            $(this).next().addClass('expand');
-        }
-        
-        else {
-            $(this).next().addClass('expand');
-        }
+// Slider
+const slider = function () {
+    const slides = document.querySelectorAll('.slide_test');
+    const btnLeft = document.querySelector('.slider__btn--left');
+    const btnRight = document.querySelector('.slider__btn--right');
+  
+    let curSlide = 0;
+    const maxSlide = slides_test.length;
+  
+    const goToSlide = function (slide_test) {
+      slides_test.forEach(
+        (s, i) => (s.style.transform = `translateX(${100 * (i - slide)}%)`)
+      );
+    };
+  
+    // Next slide
+    const nextSlide = function () {
+      if (curSlide === maxSlide - 1) {
+        curSlide = 0;
+      } else {
+        curSlide++;
+      }
+  
+      goToSlide(curSlide);
+    };
+  
+    const prevSlide = function () {
+      if (curSlide === 0) {
+        curSlide = maxSlide - 1;
+      } else {
+        curSlide--;
+      }
+      goToSlide(curSlide);
+    };
+  
+    const init = function () {
+      goToSlide(0);
+    };
+    init();
+  
+    // Event handlers
+    btnRight.addEventListener('click', nextSlide);
+    btnLeft.addEventListener('click', prevSlide);
+  
+    document.addEventListener('keydown', function (e) {
+      if (e.key === 'ArrowLeft') prevSlide();
+      e.key === 'ArrowRight' && nextSlide();
     });
-});
+  };
+  slider();
